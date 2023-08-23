a458d26ab9f9ee6fbbfa8293453ed994d79ffb6c
jm <johan.moller040@gmail.com>
2023-08-23 16:12:42 +0200

    working on product list buttons

---
+++ frontend/src/components/ConditionalButton.vue
@@ -0,0 +1,37 @@
+<template>
+    <button v-if="isVisibleIf" :class="['py-1', 'px-3', 'rounded', getBgColorClass(), customClass]" @click="handleClick">
+        {{ text }}
+    </button>
+</template>
+
+<script>
+export default {
+    props: {
+        isVisibleIf: {
+            type: Boolean,
+            required: true
+        },
+        handleClick: {
+            type: Function,
+            required: true
+        },
+        text: {
+            type: String,
+            required: true
+        },
+        color: {
+            type: String,
+            default: "gray"
+        },
+        customClass: {
+            type: String,
+            default: ""
+        }
+    },
+    methods: {
+        getBgColorClass() {
+            return `bg-${this.color}-700 hover:bg-${this.color}-800`;
+        }
+    }
+}
+</script>
--- frontend/src/product/ProductList.vue
+++ frontend/src/product/ProductList.vue
@@ -19,28 +19,19 @@
               <input v-if="product.isEditing" v-model="product.name" class="px-2 py-1 rounded mx-2 bg-gray-500" />
             </div>
 
-
-
             <div class="flex space-x-2">
 
-              <button v-if="!product.isEditing" class="py-1 px-3 bg-green-700 hover:bg-green-800 rounded"
-                @click="startEditing(product)">
-                Edit
-              </button>
+              <ConditionalButton :is-visible-if="!product.isEditing" :handle-click="() => startEditing(product)"
+                text="Edit" color="green" />
 
-              <button v-if="!product.isEditing" class="py-1 px-3 bg-red-700 hover:bg-red-800 rounded"
-                @click="handleDeleteProduct(product.id)">
-                Delete
-              </button>
+              <ConditionalButton :is-visible-if="!product.isEditing" :handle-click="() => handleDeleteProduct(product.id)"
+                text="Delete" color="red" />
 
-              <button v-if="product.isEditing" class="py-1 px-3 bg-blue-700 hover:bg-blue-800 rounded"
-                @click="handleUpdateProduct(product)">
-                Save
-              </button>
-              <button v-if="product.isEditing" class="py-1 px-3 bg-orange-700 hover:bg-orange-800 rounded"
-                @click="stopEditing(product)">
-                Cancel
-              </button>
+              <ConditionalButton :is-visible-if="!!product.isEditing && product.isEditing !== undefined"
+                :handle-click="() => handleUpdateProduct(product)" text="Save" color="blue" />
+
+              <ConditionalButton :is-visible-if="!!product.isEditing && product.isEditing !== undefined"
+                :handle-click="() => stopEditing(product)" text="Cancel" color="orange" />
 
             </div>
           </div>
@@ -51,51 +42,49 @@
 </template>
 
 <script>
+import ConditionalButton from "../components/ConditionalButton.vue";
 import { API } from "../network/API";
 
 export default {
   async created() {
     await this.updateProducts();
   },
-
   methods: {
-
     startEditing(product) {
       this.products.forEach((otherProduct) => {
         if (otherProduct.id !== product.id && otherProduct.isEditing) {
           otherProduct.isEditing = false;
         }
-      }); product.isEditing = true;
+      });
+      product.isEditing = true;
     },
-
     stopEditing(product) {
       product.isEditing = false;
     },
-
     async updateProducts() {
       try {
         const products = await API.getAllProducts();
         this.products = products;
-      } catch (error) {
+      }
+      catch (error) {
         console.error("Error updating products:", error);
       }
     },
     async handleUpdateProduct(product) {
       try {
-        await API.updateProduct(product)
+        await API.updateProduct(product);
         this.updateProducts();
-
-      } catch (error) {
+      }
+      catch (error) {
         console.error("Error updating product:", error);
       }
     },
-
     async handleDeleteProduct(productId) {
       try {
         await API.deleteProduct(productId);
         this.updateProducts();
-
-      } catch (error) {
+      }
+      catch (error) {
         console.error("Error deleting product:", error);
       }
     },
@@ -105,5 +94,6 @@ export default {
       products: [],
     };
   },
+  components: { ConditionalButton }
 };
 </script>
